8bb52ed3e1ae9bdee6fc9749e5792b52469585e1
garrettwong <gwong005@gmail.com>
2017-01-05 04:44:35 -0800

    add perceptualGraph with dragging transit effect

---
+++ verticalTree/linkModificationForm/linkModificationForm.js
@@ -0,0 +1,25 @@
+var treeDataRef;
+
+function addNode(name, url) {
+    if (!treeDataRef || !treeDataRef.children) return;
+
+    let node = {
+        name: name,
+        url: url
+    };
+
+    treeDataRef.children.push(node);
+}
+
+// event
+document.getElementById('addNode').addEventListener('click', function() {
+    let name = document.getElementById('name').value;
+    let url = document.getElementById('url').value;
+
+    if (!treeDataRef || !treeDataRef.children) return;
+
+    addNode(name, url);
+
+    // refresh
+    update(treeDataRef);
+});
