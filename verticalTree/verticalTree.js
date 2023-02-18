fadc5c3752e74ab223c632ed8ea755af7aec48bf
Garrett Wong <garrettwong@google.com>
2023-02-17 22:47:37 -0800

    fix formatting on vertical tree

--- verticalTree/verticalTree.js
+++ verticalTree/verticalTree.js
@@ -1,23 +1,22 @@
 // global ref for window.resize
-var treeDataRef;
+var treeDataRef
 
 // get data from json file
 d3.json('treeData.json', function (err, treeData) {
-    update(treeData);
+  update(treeData)
 
-    // set global json data ref
-    treeDataRef = treeData;
-});
+  // set global json data ref
+  treeDataRef = treeData
+})
 
 // resize event
-d3.select(window).on('resize', resize);
+d3.select(window).on('resize', resize)
 
 function resize() {
-    if (!treeDataRef) return;
+  if (!treeDataRef) return
 
-    update(treeDataRef);
+  update(treeDataRef)
 }
-
 
 /**
     generation of the tree graph code 
@@ -25,86 +24,113 @@ function resize() {
 
 // set the dimensions and margins of the diagram
 var margin = 40,
-    sizeReductionFactor = 1.5;
+  sizeReductionFactor = 1.5
 
 function update(treeData) {
-    d3.select('svg').selectAll("*").remove();
+  d3.select('svg').selectAll('*').remove()
 
-    var width = parseInt(d3.select('html').style('width')) / sizeReductionFactor - margin * 2,
-        height = parseInt(d3.select('html').style('height')) / sizeReductionFactor - margin * 2;
+  var width =
+      parseInt(d3.select('html').style('width')) / sizeReductionFactor -
+      margin * 2,
+    height =
+      parseInt(d3.select('html').style('height')) / sizeReductionFactor -
+      margin * 2
 
-    console.log(width, height);
+  console.log(width, height)
 
-    // declares a tree layout and assigns the size
-    var treemap = d3.tree()
-        .size([width, height]);
+  // declares a tree layout and assigns the size
+  var treemap = d3.tree().size([width, height])
 
-    //  assigns the data to a hierarchy using parent-child relationships
-    var nodes = d3.hierarchy(treeData);
+  //  assigns the data to a hierarchy using parent-child relationships
+  var nodes = d3.hierarchy(treeData)
 
-    // maps the node data to the tree layout
-    nodes = treemap(nodes);
+  // maps the node data to the tree layout
+  nodes = treemap(nodes)
 
-    // append the svg obgect to the body of the page
-    // appends a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
-    var svg = d3.select("svg")
-        .attr("width", width + margin * 2)
-        .attr("height", height + margin * 2),
-        g = svg.append("g")
-            .attr("transform",
-            "translate(" + margin + "," + margin + ")");
+  // append the svg obgect to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3
+      .select('svg')
+      .attr('width', width + margin * 2)
+      .attr('height', height + margin * 2),
+    g = svg
+      .append('g')
+      .attr('transform', 'translate(' + margin + ',' + margin + ')')
 
-    // adds the links between the nodes
-    var link = g.selectAll(".link")
-        .data(nodes.descendants().slice(1))
-        .enter().append("path")
-        .attr("class", "link")
-        .attr("d", function (d) {
-            console.log(d);
+  // adds the links between the nodes
+  var link = g
+    .selectAll('.link')
+    .data(nodes.descendants().slice(1))
+    .enter()
+    .append('path')
+    .attr('class', 'link')
+    .attr('d', function (d) {
+      console.log(d)
 
-            return "M" + d.x + "," + d.y
-                + "C" + d.x + "," + (d.y + d.parent.y) / 2
-                + " " + d.parent.x + "," + (d.y + d.parent.y) / 2
-                + " " + d.parent.x + "," + d.parent.y;
-        });
+      return (
+        'M' +
+        d.x +
+        ',' +
+        d.y +
+        'C' +
+        d.x +
+        ',' +
+        (d.y + d.parent.y) / 2 +
+        ' ' +
+        d.parent.x +
+        ',' +
+        (d.y + d.parent.y) / 2 +
+        ' ' +
+        d.parent.x +
+        ',' +
+        d.parent.y
+      )
+    })
 
-    // adds each node as a group
-    var node = g.selectAll(".node")
-        .data(nodes.descendants())
-        .enter().append("g")
-        .attr("class", function (d) {
-            return "node" +
-                (d.children ? " node--internal" : " node--leaf");
-        })
-        .attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        })
-        .on('click', openUrl);
+  // adds each node as a group
+  var node = g
+    .selectAll('.node')
+    .data(nodes.descendants())
+    .enter()
+    .append('g')
+    .attr('class', function (d) {
+      return 'node' + (d.children ? ' node--internal' : ' node--leaf')
+    })
+    .attr('transform', function (d) {
+      return 'translate(' + d.x + ',' + d.y + ')'
+    })
+    .on('click', openUrl)
 
-    function openUrl(d, i) {
-        console.log(d, i);
-        // open url
-        if (d.data.url) {
-            window.open(d.data.url);
-        } else {
-            alert('No url defined');
-        }
+  function openUrl(d, i) {
+    console.log(d, i)
+    // open url
+    if (d.data.url) {
+      window.open(d.data.url)
+    } else {
+      alert('No url defined')
     }
+  }
 
-    // adds the circle to the node
-    node.append("circle")
-        .attr("r", 10)
-        .style('fill', function (d) { 
-            var r = Math.random();
-            if (r > 0.5) return '#1f77b4';
-            return '#97b30c';
-        });
+  // adds the circle to the node
+  node
+    .append('circle')
+    .attr('r', 10)
+    .style('fill', function (d) {
+      var r = Math.random()
+      if (r > 0.5) return '#1f77b4'
+      return '#97b30c'
+    })
 
-    // adds the text to the node
-    node.append("text")
-        .attr("dy", ".35em")
-        .attr("y", function (d) { return d.children ? -20 : 20; })
-        .style("text-anchor", "middle")
-        .text(function (d) { return d.data.name; });
+  // adds the text to the node
+  node
+    .append('text')
+    .attr('dy', '.35em')
+    .attr('y', function (d) {
+      return d.children ? -20 : 20
+    })
+    .style('text-anchor', 'middle')
+    .text(function (d) {
+      return d.data.name
+    })
 }
